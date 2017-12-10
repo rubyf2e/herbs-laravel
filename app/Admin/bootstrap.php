fae4517a6be22bd4661b412cd6d0ad30946f845d
Ruby <joy511437@gmail.com>
2017-12-10 17:21:55 +0800

    引入laravel-admin.org extensions組件

--- app/Admin/bootstrap.php
+++ app/Admin/bootstrap.php
@@ -17,5 +17,27 @@
  * Admin::js('/packages/prettydocs/js/main.js');
  *
  */
+use App\Admin\Extensions\Column\ExpandRow;
+use App\Admin\Extensions\Column\OpenMap;
+use App\Admin\Extensions\Column\FloatBar;
+use App\Admin\Extensions\Column\Qrcode;
+use App\Admin\Extensions\Column\UrlWrapper;
+use App\Admin\Extensions\Form\WangEditor;
+use App\Admin\Extensions\Nav\Links;
+use Encore\Admin\Grid;
+use Encore\Admin\Form;
+use Encore\Admin\Facades\Admin;
+use Encore\Admin\Grid\Column;
 
-Encore\Admin\Form::forget(['map', 'editor']);
+Column::extend('expand', ExpandRow::class);
+Column::extend('openMap', OpenMap::class);
+Column::extend('floatBar', FloatBar::class);
+Column::extend('qrcode', Qrcode::class);
+Column::extend('urlWrapper', UrlWrapper::class);
+Column::extend('action', Grid\Displayers\Actions::class);
+
+Column::extend('prependIcon', function ($value, $icon) {
+
+    return "<span style='color: #999;'><i class='fa fa-$icon'></i>  $value</span>";
+
+});
