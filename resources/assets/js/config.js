bfde9070dfe3bf1ee93cdbe8e8306176279a7dce
Ruby <rubyf2e@gmail.com>
2021-04-22 01:09:54 +0800

    改網址

--- resources/assets/js/config.js
+++ resources/assets/js/config.js
@@ -1,11 +1,11 @@
 const CONFIG = {
 	XSRF_TOKEN: decodeURIComponent(document.cookie.replace(/(?:(?:^|.*;\s*)XSRF-TOKEN\s*\=\s*([^;]*).*$)|^.*$/, "$1")),
-	PHOTO_URL: 'https://rubyherbs.cloud/',
-	API_URL:'https://rubyherbs.cloud/api/',
-	TAB_API:'https://rubyherbs.cloud/api/'+'maps',
-	SLIDER_API:'https://rubyherbs.cloud/api/'+'sliders',
-	SIDEBAR_API:'https://rubyherbs.cloud/api/'+'sidebars',
-	DETAIL_API:'https://rubyherbs.cloud/api/'+'details'
+	PHOTO_URL: 'https://rubyworks.site/herbs-laravel/public/',
+	API_URL:'https://rubyworks.site/herbs-laravel/public/api/',
+	TAB_API:'https://rubyworks.site/herbs-laravel/public/api/'+'maps',
+	SLIDER_API:'https://rubyworks.site/herbs-laravel/public/api/'+'sliders',
+	SIDEBAR_API:'https://rubyworks.site/herbs-laravel/public/api/'+'sidebars',
+	DETAIL_API:'https://rubyworks.site/herbs-laravel/public/api/'+'details'
 }
 
 export { CONFIG };
